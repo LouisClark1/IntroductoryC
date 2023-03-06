138ccc4631dc22558972911f9c829ec7934c8ec2
LouisClark1 <louis.clark2410@gmail.com>
2023-03-06 14:57:53 +0000

    created file of example data types

---
+++ DataTypes.c
@@ -0,0 +1,20 @@
+// standard input output
+#include <stdio.h>
+int main(){    
+    //type conversions
+    float exampleFloat = 12.5568;
+    int exampleInt = 0;
+    unsigned int a;
+    exampleInt = exampleFloat;  //the int becomes 12 because it is truncated
+    printf("equating an int type to a float of 12.5568 gives %i\n", exampleInt);
+
+    //casting has almost the highest precedence
+    a = (int)21.55 + (int)24.9;
+    printf("a = (int)21.55 + (int)24.9 =%i\n", a);
+    // when doing arithmetic operations on different data types the less precise values are promoted to match the highest
+    float b = exampleFloat * exampleInt;
+    printf("(float)12.5568 + (int)13 = %lf\n", b);
+
+    //size of is a keyword to tell us how many bites are used, is an operator not a function
+    printf("sizeof(int) = %li, this shows the number of bytes not bits\n\n", sizeof(a));
+}
